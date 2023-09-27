ec00a866a9178245487c7ef8bd76eab4252b34fa
anthony <tony20402014@gmail.com>
2023-09-26 21:12:24 -0600

    añadiendo todo

---
+++ app/src/main/java/com/example/intens/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.intens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
